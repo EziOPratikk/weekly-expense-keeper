e43d7e58c2cf6f513f471806c30dd67c36f32a0e
Pratik <khadkapratikk10@gmail.com>
2025-01-14 19:03:59 +0545

    feat: add toast notification functionality

--- src/pages/NewTransaction.tsx
+++ src/pages/NewTransaction.tsx
@@ -1,24 +1,34 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-import NewTransactionForm from '../components/NewTransactionForm.tsx';
 import { type Transaction } from '../model/transaction.tsx';
+import NewTransactionForm from '../components/NewTransactionForm.tsx';
 import { useTransactionsContext } from '../hooks/useTransactionsContext.tsx';
+import Toast from '../utils/Toast.tsx';
 
 export default function NewTransaction() {
-  const navigate = useNavigate();
+  const [isToastOpen, setIsToastOpen] = useState(false);
 
   const { addTransaction } = useTransactionsContext();
 
   function addTransactionItem(transaction: Transaction) {
     addTransaction(transaction);
 
-    navigate('/');
+    setIsToastOpen(true);
+    setTimeout(() => {
+      setIsToastOpen(false);
+    }, 2000);
   }
 
   return (
     <section>
       <h1 className='text-center font-bold text-2xl'>EXPENSE ENTRY</h1>
       <NewTransactionForm onAdd={addTransactionItem} />
+      {isToastOpen && (
+        <Toast
+          message='Expense added successfully!'
+          type='success'
+        />
+      )}
     </section>
   );
 }
